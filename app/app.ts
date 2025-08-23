65da0e196af95e2997f91fb4ae50295ea7f48c6c
daniel castaneda <daniel.castaneda@davivienda.com>
2025-08-23 14:31:25 -0500

    Integrate OpenAI and Twilio for WhatsApp messaging; update dependencies

--- app/app.ts
+++ app/app.ts
@@ -1,18 +1,52 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
 import express from 'express';
+import twilio from 'twilio';
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const openAikey = process.env.OPENAI_KEY;
+
+const client = twilio(accountSid, authToken);
+const openai = new OpenAI({
+    baseURL: 'https://openrouter.ai/api/v1',
+    apiKey: openAikey,
+});
+
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/message-recived', (req, res) => {
-    console.log('=== Body recibido ===');
-    console.log(req.body); 
+app.post('/message-recived', async (req, res) => {
+    const { ProfileName, Body, WaId } = req.body
 
-    console.log('=== Params ===');
-    console.log(JSON.stringify(req.params));
+    const response = await openai.chat.completions.create({
+        model: 'openai/gpt-4o',
+        messages: [
+            {
+                role: 'user',
+                content: Body,
+            },
+            {
+                role: 'developer',
+                content: 'you are a assistant from Habi Broker platform',
+            },
+        ],
+    });
+    const messageToResponse = response.choices[0].message.content;
 
-    res.send('OK');
+    //SEND A RESPONSE MESSAGE
+
+    const message = await client.messages.create({
+        body: messageToResponse,
+        from: 'whatsapp:+14155238886',
+        to: `whatsapp:+${WaId}`,
+    });
+
+    console.log(message.sid);
+    res.send();
 });
 
 app.get('/', (_, res) => res.send('OK'));
